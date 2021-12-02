c33dc80f7f308c79fa6660cafc856b72fccb8fe2
heloisacamboim <heloisacamboim@gmail.com>
2021-12-02 11:47:59 -0300

    correcao

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+
 let snake = [];
 snake[0] = {
     x: 8 * box,
@@ -14,7 +15,7 @@ let food = {
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0 ,16 * box, 16 * box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
@@ -33,10 +34,10 @@ function drawFood() {
 document.addEventListener("keydown", update);
 
 function update(event) {
-    if (event.keyCode == 37 && direction != "right") direction == "left";
-    if (event.keyCode == 38 && direction != "down") direction == "up";
-    if (event.keyCode == 39 && direction != "left") direction == "right";
-    if (event.keyCode == 40 && direction != "up") direction == "down";
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo() {
